036b401ed709337f61356fa978ec8e7a894bb332
Nick Galetti <nga@codehousegroup.com>
2014-02-14 15:12:58 +0000

    Used autofac to register documentstore on app_start.

--- PusherMvc.Web/Controllers/StoreController.cs
+++ PusherMvc.Web/Controllers/StoreController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Raven.Client;
 
 namespace PusherMvc.Web.Controllers
 {
@@ -13,6 +14,7 @@ namespace PusherMvc.Web.Controllers
 
         public ActionResult Index()
         {
+
             return View();
         }
 
--- PusherMvc.Web/Global.asax.cs
+++ PusherMvc.Web/Global.asax.cs
@@ -1,7 +1,13 @@
-﻿using PusherMvc.Web.Repositories;
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using Autofac.Integration.Mvc;
+using PusherMvc.Web.Repositories;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Raven.Client;
+using Raven.Client.Embedded;
 
 namespace PusherMvc.Web
 {
@@ -11,15 +17,50 @@ namespace PusherMvc.Web
     {
         protected void Application_Start()
         {
+            #region Autofac
+
+            var builder = new ContainerBuilder();
+            
+            #region RavenDB
+            
+            //register document store as single instance
+            builder.Register(ds =>
+            {
+                var documentStore = new EmbeddableDocumentStore
+                {
+                    ConnectionStringName = "RavenDB",
+                    //by default ravendb separate parts with / but this could cause a problem with the routing engine
+                    Conventions = {IdentityPartsSeparator = "-"}
+                };
+
+                documentStore.Initialize();
+                return documentStore;
+            }).As<IDocumentStore>().SingleInstance();
+
+            //register session and open it
+            builder.Register(s =>
+                s.Resolve<IDocumentStore>().OpenSession()
+                ).As<IDocumentSession>().InstancePerLifetimeScope().OnRelease(sr =>
+                {
+                    sr.SaveChanges();
+                    sr.Dispose();
+                });
+
+            #endregion
+
+            //TODO: register controllers to leverage autofac MVC
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            #endregion
+
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-
-            //initialize Document store. We want to do it in app start as a singleton because it is an expensive operation
-            DataDocumentStore.Initialize();
-
         }
     }
 }
